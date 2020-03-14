615ecc0e9bb9e63eae968059c4821ccfb2b645f3
Edianio Ferreira <edianioferreira2@gmail.com>
2020-03-14 10:26:50 -0300

    adicionado flatList e funcao callBack

--- Tarefas/src/components/Task.js
+++ Tarefas/src/components/Task.js
@@ -1,19 +1,26 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, TouchableWithoutFeedback } from 'react-native'
 import Icon from 'react-native-vector-icons/FontAwesome'
+import moment from 'moment'
+import 'moment/locale/pt-br'
 
 import commonStyles from '../commonStyles'
 
 export default props => {
+    const doneOrNotStyle = props.doneAt != null ? { textDecorationLine: 'line-through' } : {}
+    const date = props.doneAt ? props.doneAt : props.estimateAt
+    const formattedDate = moment(date).locale('pt-br').format('ddd, D [de] MMMM')
+
     return (
         <View style={styles.container}>
-            <View style={styles.checkContainer} >
-                {getCheckView(props.doneAt)}
-            </View>
+            <TouchableWithoutFeedback onPress={() => props.toggleTask(props.id)}>
+                <View style={styles.checkContainer} >
+                    {getCheckView(props.doneAt)}
+                </View>
+            </TouchableWithoutFeedback>
             <View  >
-                <Text>{props.desc}</Text>
-                <Text>{props.estimateAt + ""}</Text>
-                <Text>{props.doneAt + ""}</Text>
+                <Text style={[styles.desc, doneOrNotStyle]} >{props.desc}</Text>
+                <Text style={styles.date} >{formattedDate + ""}</Text>
             </View>
         </View>
     )
@@ -60,5 +67,15 @@ const styles = StyleSheet.create({
         backgroundColor: '#4d7031',
         alignItems: 'center',
         justifyContent:'center',
+    },
+    desc: {
+        fontFamily: commonStyles.fontFamily,
+        color: commonStyles.colors.mainText,
+        fontSize: 15,
+    },
+    date: {
+        fontFamily: commonStyles.fontFamily,
+        color: commonStyles.colors.subText,
+        fontSize: 12,
     }
 })
--- Tarefas/src/screens/TaskList.js
+++ Tarefas/src/screens/TaskList.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
-import { View, Text, ImageBackground,StyleSheet } from 'react-native'
+import { View, Text, ImageBackground, StyleSheet, TouchableOpacity, Platform } from 'react-native'
+import { FlatList } from 'react-native-gesture-handler'
+import Icon from 'react-native-vector-icons/FontAwesome'
 
 import commonStyles from '../commonStyles'
 import todayImage from '../../assets/imgs/today.jpg'
@@ -9,19 +11,55 @@ import moment from 'moment'
 import 'moment/locale/pt-br'
 
 export default class TaskList extends Component {
+    state = {
+        showDoneTasks: true,
+        tasks: [{
+            id: Math.random(),
+            desc: 'Comprar Livro',
+            estimateAt: new Date(),
+            doneAt: new Date(),
+        }, {
+            id: Math.random(),
+            desc: 'Ler Livro',
+            estimateAt: new Date(),
+            doneAt: null,
+        }]
+    }
+
+    toggleFilter = () => {
+        this.setState({showDoneTasks: !this.state.showDoneTasks})
+    }
+
+    toggleTask = taskId => {
+        const tasks = [...this.state.tasks]
+        tasks.forEach(task => {
+            if(task.id === taskId){
+                task.doneAt = task.doneAt ? null : Date()
+            }
+        })
+
+        this.setState({tasks})
+    }
+
     render(){
         const today = moment().locale('pt-br').format('ddd, D [de] MMMM')
         return (
             <View style={styles.container}>
                 <ImageBackground source={todayImage} style={styles.background}>
+                    <View style={styles.iconBar} >
+                        <TouchableOpacity onPress={this.toggleFilter} >
+                            <Icon name={this.state.showDoneTasks ? 'eye' : 'eye-slash'} size={20} color={commonStyles.colors.secondary} />
+                        </TouchableOpacity>
+                    </View>
                     <View style={styles.titleBar}>
                         <Text style={styles.title} >Hoje</Text>
                         <Text style={styles.subtitle} >{today}</Text>
                     </View>
                 </ImageBackground>
                 <View style={styles.taskList}>
-                    <Task desc="Comprar Livro" estimateAt={new Date()} doneAt={new Date()} />
-                    <Task desc="Comprar Livro" estimateAt={new Date()} doneAt={null} />
+                    <FlatList data={this.state.tasks}
+                        keyExtractor={item => `${item.id}`}
+                        renderItem={({item}) => <Task {...item} toggleTask={this.toggleTask} />} />
                 </View>
             </View>
         )
@@ -55,5 +93,11 @@ const styles = StyleSheet.create({
         fontSize: 20,
         marginLeft: 20,
         marginBottom: 30,
+    },
+    iconBar: {
+        flexDirection: 'row',
+        marginHorizontal: 20,
+        justifyContent: 'flex-end',
+        marginTop: Platform.OS === 'ios' ? 40 : 10,
     }
 })
